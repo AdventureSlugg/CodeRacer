cbbe9c86c623c7bd3f7d3e214ab1c518cb75fd90
jltlam <jlam011235@gmail.com>
2024-11-02 22:19:39 -0400

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,22 @@
-/**
- * Nothing yet but server side will go in here
- */
+
+
+const server = require('node:http').createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+const io = require('socket.io')(server, {});
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+io.on('connection', (socket) => {
+  socket.emit("user connected")
+  socket.broadcast('')
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
