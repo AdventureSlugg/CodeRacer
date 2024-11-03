3ec22e3b0753baf1555b17ae66ca3a45c96bc848
Zoe <zmb6893@g.rit.edu>
2024-11-03 05:56:50 -0500

    Added in styling for accuracy

--- frontend/src/components/ProgrammingInterface.vue
+++ frontend/src/components/ProgrammingInterface.vue
@@ -10,7 +10,17 @@
 
 		<div>
 			<p id="solutionCodeOverlay"> {{ solutionCode }} </p>
-			<textarea id="codeContent"></textarea>
+			<textarea id="codeContent" v-model="writtenCode"></textarea>
+			<p id="accuracyOverlay">
+				<span v-for="(char, index) in writtenCode" :key="index">
+					<span :class="{
+						'correct': writtenCode[index] === solutionCode[index],
+						'incorrect': writtenCode[index] !== solutionCode[index]
+					}">
+						{{ char }}
+					</span>
+				</span>
+			</p>
 		</div>
 		
 	</div>
@@ -21,15 +31,16 @@ import { onMounted, ref } from 'vue';
 
 const lines = ref([])
 const solutionCode = '// This is the solution'
+const writtenCode = ref('');
 
-const calculateLineCount = () => {
+const generateLineNumbers = () => {
 	const codeContentHeight = document.getElementById("codeContent").offsetHeight - 8;
 	const lineCount = Math.floor(codeContentHeight / 22)
 	lines.value = Array.from({length: lineCount}, (_, i) => i + 1)
 }
 
 onMounted( () => {
-	calculateLineCount()
+	generateLineNumbers()
 })
 </script>
 
@@ -71,10 +82,32 @@ onMounted( () => {
 		background-color: #1F1F1F;
 		color: #4b4b4b;
 		resize: none;
-		z-index: 1;
+		z-index: 100;
 		position: absolute;
 		font-size: 22px;
 		font-family: 'Courier New', Courier, monospace;
+	}
+
+	#accuracyOverlay {
+		width: calc(100% - 20px - 5rem);
+		height: calc(100% - 20px);
+		background: rgba(0,0,0,0);
+		resize: none;
+		z-index: 300;
+		position: absolute;
+		padding: 8px;
+		font-size: 22px;
+		font-family: 'Courier New', Courier, monospace;
+		border: none;
+		pointer-events: none;
+	}
+
+	.correct {
+		color: #00c81e;
+	}
+
+	.incorrect {
+		color: #aa2f2f;
 	}
 
 	p {
