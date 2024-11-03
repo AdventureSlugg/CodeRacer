0061cc9523919639187e861da5de6710652b7fd9
jltlam <jlam011235@gmail.com>
2024-11-03 00:34:19 -0400

    users can connect and send word count to server

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,48 @@ const server = require('node:http').createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
-const io = require('socket.io')(server, {});
+const io = require('socket.io')(server, {
+  // allowRequest: (req, callback) => {
+  //   const noOriginHeader = req.headers.origin === undefined;
+  //   callback(null, noOriginHeader);
+  // }
+  cors: {
+    origin: "*",
+    // origin: "http://127.0.0.1:8080",
+    methods: ["GET", "POST"],
+  }
+});
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+let uid = 0;
+
+const scores = new Map();
+
 io.on('connection', (socket) => {
-  socket.emit("user connected")
-  socket.broadcast('')
+  console.log(socket.id);
+  socket.emit('assignUid', uid);
+  uid++;
+  scores.set(uid, 0);
+
+  socket.on('start', () => {
+
+  })
+
+  socket.on('wordcount', (uid, count) => {
+    console.log(uid, count);
+    scores.set(uid, count);
+  })
+
+  // // broadcast every 200 ms. maybe should be out of this connect
+  // socket.broadcast('scores', scores);
+});
+
+
+io.on('disconnect', (socket) => {
+  socket.emit('damn, this guy left')
+  console.log(socket.id); // undefined
 });
 
 server.listen(port, hostname, () => {
--- frontend/src/pages/RaceView.vue
+++ frontend/src/pages/RaceView.vue
@@ -5,10 +5,40 @@
 		</template>
 		<template #main-content>
 			hello
+			<button @click="sendCount">yolosdflkdsjf</button>
+			<button @click="sendStartGame">Start Game</button>
 		</template>
 	</SplitContentFocused>
 </template>
 
 <script setup>
 import SplitContentFocused from '@/layouts/SplitContentFocused.vue';
+const { io } = require("socket.io-client");
+
+const cio = io('http://127.0.0.1:3000/');
+let count = 0;
+let uid = 0; // this needs to be set sometime
+// client-side
+cio.on('connect', () => {
+	cio.on('assignUid', (who) => {
+		uid = who;
+		console.log('connected! you are ' + uid)
+	})
+});
+
+function sendCount() {
+	count++;
+	cio.emit('wordcount', uid, count);
+	console.log('ahhhhhhhh');
+}
+
+function sendStartGame() {
+	count++;
+	cio.emit('start');
+	console.log('start!!!');
+}
+
+
+
+
 </script>
