56adeac5c3ec0501e75481fddc17e1411c51814f
Zoe <zmb6893@g.rit.edu>
2024-11-03 06:40:07 -0500

    Added in the accuracy, wordcount, and timer

--- frontend/src/components/ProgrammingInterface.vue
+++ frontend/src/components/ProgrammingInterface.vue
@@ -1,4 +1,17 @@
 <template>
+	<div class="status">
+		<p>
+			Timer: {{ timer }} s
+		</p>
+
+		<p>
+			Speed: {{ wordsPerMinute }} wpm
+		</p>
+		<p>
+			Accuracy: {{ accuracy.toFixed(2) }} %
+		</p>
+		
+	</div>
 	<div class="programming-interface">
 		<table>
 			<tbody>
@@ -10,28 +23,84 @@
 
 		<div>
 			<p id="solutionCodeOverlay"> {{ solutionCode }} </p>
-			<textarea id="codeContent" v-model="writtenCode"></textarea>
+			<textarea id="codeContent" v-model="writtenCode" @input="updateStats()"></textarea>
 			<p id="accuracyOverlay">
 				<span v-for="(char, index) in writtenCode" :key="index">
 					<span :class="{
 						'correct': writtenCode[index] === solutionCode[index],
 						'incorrect': writtenCode[index] !== solutionCode[index]
 					}">
-						{{ char }}
+						{{ solutionCode[index] }}
 					</span>
 				</span>
 			</p>
 		</div>
-		
 	</div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 
 const lines = ref([])
 const solutionCode = '// This is the solution'
 const writtenCode = ref('');
+
+// Status
+const wordsPerMinute = ref(0);
+const timer = ref('00:00');
+const accuracy = ref(0);
+const totalTimeInSeconds = ref(0);
+
+let timerInterval;
+let incorrectChars = 0;
+let hasStarted = false;
+
+const updateStats = () => {
+	if (hasStarted == false) {
+		hasStarted = true;
+		// Start the timer
+		timerInterval = setInterval(() => {
+			totalTimeInSeconds.value += 1;
+			formatTime();
+		}, 1000);
+	}
+	updateAccuracy();
+	calculateWPM();
+}
+
+const calculateWPM = () => {
+	const wordCount = writtenCode.value.trim().split(/\s+/).length;
+	const minutes = totalTimeInSeconds.value / 60;
+	wordsPerMinute.value = minutes > 0 ? (wordCount / minutes).toFixed(2) : 0;
+}
+
+const updateAccuracy = () => {
+	const lengthOfWrittenCode = writtenCode.value.length;
+
+	// Reset incorrectChars
+	incorrectChars = 0;
+
+	// Calculate incorrect characters
+	for (let i = 0; i < lengthOfWrittenCode; i++) {
+		if (writtenCode.value[i] !== solutionCode[i]) {
+			incorrectChars += 1;
+		}
+	}
+
+	// Calculate accuracy, avoiding division by zero
+	accuracy.value = (lengthOfWrittenCode > 0 
+		? (lengthOfWrittenCode - incorrectChars) / lengthOfWrittenCode
+		: 1) * 100;
+
+	formatTime();
+};
+
+const formatTime = () => {
+	const minutes = Math.floor(totalTimeInSeconds.value / 60).toString().padStart(2, '0');
+	const seconds = (totalTimeInSeconds.value % 60).toString().padStart(2, '0')
+
+	timer.value = `${minutes}:${seconds}`
+}
 
 const generateLineNumbers = () => {
 	const codeContentHeight = document.getElementById("codeContent").offsetHeight - 8;
@@ -40,7 +109,11 @@ const generateLineNumbers = () => {
 }
 
 onMounted( () => {
-	generateLineNumbers()
+	generateLineNumbers();
+})
+
+onUnmounted( () => {
+	clearInterval(timerInterval);
 })
 </script>
 
@@ -113,6 +186,21 @@ onMounted( () => {
 	p {
 		margin: 0;
 		padding: 9px;
+	}
+}
+
+.status {
+	color: #00A2BE;
+	text-align: right;
+	width: 100%;
+	display: flex;
+	flex-direction: row-reverse;
+	position: relative;
+	height: 0rem;
+	padding: 0rem;
+	top: -2rem;
+	p {
+		margin: 0rem 1rem;
 	}
 }
 </style>
